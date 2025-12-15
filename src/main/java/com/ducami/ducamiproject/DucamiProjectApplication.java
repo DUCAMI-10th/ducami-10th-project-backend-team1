e0365320afd4594697ffc966978d435b98358464
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-12-15 19:29:59 +0900

    feat: 초기 커밋

---
+++ src/main/java/com/ducami/ducamiproject/DucamiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ducami.ducamiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucamiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DucamiProjectApplication.class, args);
+    }
+
+}
