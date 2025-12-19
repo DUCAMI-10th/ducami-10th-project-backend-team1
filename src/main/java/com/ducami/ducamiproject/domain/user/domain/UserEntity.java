4fd5ad3f2a94a77156cd490eb5631d648f8a8df5
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-12-19 14:07:28 +0900

    feat: userEntity 추가

---
+++ src/main/java/com/ducami/ducamiproject/domain/user/domain/UserEntity.java
@@ -0,0 +1,10 @@
+package com.ducami.ducamiproject.domain.user.domain;
+
+import com.ducami.ducamiproject.global.entity.Base;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends Base {
+}
