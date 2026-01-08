f1658929cdbc993433869e7e557dae57f2dafd60
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2026-01-08 16:26:08 +0900

    chore: Helpder 수정

--- src/test/java/com/ducami/ducamiproject/global/security/AuthenticationHelper.java
+++ src/test/java/com/ducami/ducamiproject/global/security/AuthenticationHelper.java
@@ -1,27 +1,23 @@
 package com.ducami.ducamiproject.global.security;
 
+import com.ducami.ducamiproject.domain.user.domain.UserEntity;
+import com.ducami.ducamiproject.global.security.entity.CustomUserDetails;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AuthenticationHelper<U> {
+public class AuthenticationHelper {
 
-    private Class<U> clazz;
-
-    public AuthenticationHelper(Class<U> clazz) {
-        this.clazz = clazz;
-    }
-
-    public U getCurrentUser() {
+    public UserEntity getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null) {
             return null;
         }
 
         Object principal = authentication.getPrincipal();
-        if (clazz.isInstance(principal)) {
-            return clazz.cast(principal);
+        if (principal instanceof CustomUserDetails) {
+            return (UserEntity) ((CustomUserDetails) principal).getUser();
         }
         return null;
     }
